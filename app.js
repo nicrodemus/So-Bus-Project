c0beadfaa0fb553d487582bbb962f3c156b994ba
Niccolò Boscolo <niccolo.boscolo88@icloud.com>
2019-04-09 02:43:42 +0200

    ready

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.listen(process.env.port || 4000);
+app.listen(process.env.port || 3000);
 console.log("Running at Port 3000");
 // catch 404 and forward to error handler
 
